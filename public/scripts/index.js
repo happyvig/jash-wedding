4c97dadb97d138e2ed1e7c825a682a69ca0b597e
VignesH KumaR <kvignsh@gmail.com>
2018-01-17 17:29:25 +0530

    Initial public subtree commit

---
+++ public/scripts/index.js
@@ -0,0 +1,12 @@
+
+// scroll to page top on load
+//jQuery(function($) {
+   //setTimeout(function() {
+        //$( 'html, body' ).animate( { scrollTop: 0 }, 0 );
+  // }, 250);
+//});
+
+$(document).ready(function(){
+  $('.slider').bxSlider();
+});
+
