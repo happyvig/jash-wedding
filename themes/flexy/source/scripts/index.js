bbc7be82e33da69c98ca0705999e9dcf0c9fd701
VignesH KumaR <kvignsh@gmail.com>
2018-01-18 10:55:08 +0530

    Added scratch card

--- themes/flexy/source/scripts/index.js
+++ themes/flexy/source/scripts/index.js
@@ -1,12 +1,15 @@
 
 // scroll to page top on load
 //jQuery(function($) {
-   //setTimeout(function() {
-        //$( 'html, body' ).animate( { scrollTop: 0 }, 0 );
-  // }, 250);
+//setTimeout(function() {
+//$( 'html, body' ).animate( { scrollTop: 0 }, 0 );
+// }, 250);
 //});
 
-$(document).ready(function(){
+
+jQuery(function ($) {
+
+  //Init pictures carousel
   $('.slider').bxSlider({
     mode: 'fade',
     captions: false,
@@ -18,5 +21,28 @@ $(document).ready(function(){
     shrinkItems: true,
     speed: 1000
   });
+
+  // Create a new scratch card on load
+  const scContainer = document.getElementById('js--sc--container')
+  const sc = new ScratchCard('#js--sc--container', {
+    scratchType: SCRATCH_TYPE.CIRCLE,
+    containerWidth: scContainer.offsetWidth,
+    containerHeight: 300,
+    imageForwardSrc: '/images/scratcher.jpg',
+    imageBackgroundSrc: '/images/carousel-r-1.jpg',
+    clearZoneRadius: 30,
+    percentToFinish: 90, // When the percent exceeds 90 on touchend event the callback will be exec.
+    callback: function () {
+      // alert('Success !!')
+    }
+  })
+  sc.init().then(() => {
+    sc.canvas.addEventListener('scratch.move', () => {
+      this.percent = sc.getPercent().toFixed(2)
+    })
+  })
+
+
+
 });
 
