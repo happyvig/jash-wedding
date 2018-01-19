f97e5751e8f464568d6d0d54c5962f4d4c0884c9
VignesH KumaR <kvignsh@gmail.com>
2018-01-19 11:00:04 +0530

    Flip card replaces scratch card

--- themes/flexy/source/scripts/index.js
+++ themes/flexy/source/scripts/index.js
@@ -23,25 +23,10 @@ jQuery(function ($) {
   });
 
   // Create a new scratch card on load
-  const scContainer = document.getElementById('js--sc--container')
-  const sc = new ScratchCard('#js--sc--container', {
-    scratchType: SCRATCH_TYPE.CIRCLE,
-    containerWidth: scContainer.offsetWidth,
-    containerHeight: 300,
-    imageForwardSrc: '/images/scratcher.jpg',
-    imageBackgroundSrc: '/images/carousel-r-1.jpg',
-    clearZoneRadius: 30,
-    percentToFinish: 90, // When the percent exceeds 90 on touchend event the callback will be exec.
-    callback: function () {
-      // alert('Success !!')
-    }
-  })
-  sc.init().then(() => {
-    sc.canvas.addEventListener('scratch.move', () => {
-      this.percent = sc.getPercent().toFixed(2)
-    })
-  })
 
+  $('section.card-container').on('click', function () {
+    $('section.card-container').find('#card').toggleClass('flipped');
+  });
 
 
 });
